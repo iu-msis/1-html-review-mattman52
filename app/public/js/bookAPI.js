6a4e1ee7840673864336992839ea3ffe97ddc4fd
Matthew Cummings <mfcummin@iu.edu>
2021-10-10 16:39:53 -0400

    Updating Docker

---
+++ app/public/js/bookAPI.js
@@ -0,0 +1,48 @@
+
+const SomeApp = {
+    data() {
+      return {
+          books: []
+      }
+    },
+    computed: {},
+    methods: {
+        prettyDollar(n) {
+            const d = new Intl.NumberFormat("en-US").format(n);
+            return "$ " + d;
+        },
+        fetchBookData() {
+            fetch('/api/books/')
+            .then( response => response.json() )
+            .then( (responseJson) => {
+                console.log(responseJson);
+                this.students = responseJson;
+            })
+            .catch( (err) => {
+                console.error(err);
+            })
+        },
+        fetchOfferData(s) {
+            console.log("Fetching offer data for ", s);
+            fetch('/api/offer/?student=' + s.id)
+            .then( response => response.json() )
+            .then( (responseJson) => {
+                console.log(responseJson);
+                this.offers = responseJson;
+            })
+            .catch( (err) => {
+                console.error(err);
+            })
+            .catch( (error) => {
+                console.error(error);
+            });
+        }
+    },
+    created() {
+        this.fetchStudentData();
+    }
+  
+  }
+  
+  Vue.createApp(SomeApp).mount('#offerApp');
+  
